fc6cc478e6a33582ee297e83904f8d9fc7d537d9
Pierre Racine <pierreracine70@gmail.com>
2013-09-27 11:54:24 -0400

    Documentation stays in the main file (noy in the Readme).

--- README.md
+++ README.md
@@ -4,31 +4,6 @@ http://github.com/pedrogit/postgisaddons
 
 The PostGIS add-ons attempt to gathers, in a single .sql file, useful and 
 generic user contributed PL/pgSQL functions and to provide a fast and Agile 
-way to release them. The last version number is the actual version of the file. 
-It should ALWAYS be left in a stable, installable and tested state .
-Function signature and return values should not change from minor revision to 
-minor revision.
+way to release them.
 
-To be included a function:
-
-  - Must be of generic enough to be useful to many PostGIS users.
-  - Must be documented according to the rules defined in this file.
-  - Must be accompagned by a series of test in the postgis_addons_test.sql file
-  - Must have a drop statement in the postgis_addons_uninstall.sql file.
-
-Companion files
-
-postgis_addons.sql             -- Main redistributable file containing all the functions.
-postgis_addons_uninstall.sql   -- Uninstallation file.
-postgis_addons_test.sql        -- Self contained test file to be executed after installation 
-                                  and before any commit of the main file.
-Mandatory help elements for each function
-
-Function name,
-Parameters listing and description of each parameter,
-Description,
-A self contained example,
-A typical, not necessarily self contained, example,
-Links to more examples on the web (blog post, etc...),
-Authors names with emails,
-Date and version of availability (date of inclusion in PostGIS Add-ons).
+More documentation to be found in the main postgis_addons.sql file.
--- postgis_addons.sql
+++ postgis_addons.sql
@@ -5,34 +5,42 @@
 -- 
 -- The PostGIS add-ons attempt to gathers, in a single .sql file, useful and 
 -- generic user contributed PL/pgSQL functions and to provide a fast and Agile 
--- way to release them. The last version number is the actual version of the file. 
--- It should ALWAYS be left in a stable, installable and tested state .
--- Function signature and return values should not change from minor revision to 
+-- way to release them. The last version number of the postgis_addons.sql file
+-- is the actual version of the product. 
+-- This file should ALWAYS be left in a stable, installable and tested state.
+-- Function signatures and return values should not change from minor revision to 
 -- minor revision.
 --
 -- To be included a function:
---
+-- 
 --   - Must be of generic enough to be useful to many PostGIS users.
---   - Must be documented according to the rules defined in this file.
+--   - Must be documented according to the rules defined in this readme file.
 --   - Must be accompagned by a series of test in the postgis_addons_test.sql file
 --   - Must have a drop statement in the postgis_addons_uninstall.sql file.
---
+-- 
 -- Companion files
---
--- postgis_addons.sql             -- Main redistributable file containing all the functions.
--- postgis_addons_uninstall.sql   -- Uninstallation file.
--- postgis_addons_test.sql        -- Self contained test file to be executed after installation 
---                                   and before any commit of the main file.
--- Mandatory help elements for each function
---
--- Function name,
--- Parameters listing and description of each parameter,
--- Description,
--- A self contained example,
--- A typical, not necessarily self contained, example,
--- Links to more examples on the web (blog post, etc...),
--- Authors names with emails,
--- Date and version of availability (date of inclusion in PostGIS Add-ons).
+-- 
+--   - postgis_addons.sql             Main redistributable file containing all the functions.
+--   - postgis_addons_uninstall.sql   Uninstallation file.
+--   - postgis_addons_test.sql        Self contained test file to be executed after installation 
+--                                       and before any commit of the main file.
+-- 
+-------------------------------------------------------------------------------
+-- Documentation
+-- 
+-- Each function must be documented directly in the postgis_addons.sql file just 
+-- before the definition of the function.
+-- 
+-- Mandatory documentation elements for each function:
+-- 
+--   - Function name,
+--   - Parameters listing and description of each parameter,
+--   - Description,
+--   - A self contained example,
+--   - A typical, not necessarily self contained, example,
+--   - Links to more examples on the web (blog post, etc...),
+--   - Authors names with emails,
+--   - Date and version of availability (date of inclusion in PostGIS Add-ons).
 -------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
